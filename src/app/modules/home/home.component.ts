001e2f8bd4bbdb65e3c88dbf13885c1d139e12a9
Leandro Machado Pereira <llpereiras@gmail.com>
2017-01-03 19:42:31 -0200

    Retirando variavel usuario_nome que foi adicionada ao app.component.ts

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -5,12 +5,5 @@ import { SessionStorageService } from 'ng2-webstorage';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-
   title = 'Home';
-  usuario_nome = '';
-
-  constructor(private sessionSt:SessionStorageService){
-    // Verificar se é possível que a api retorne o nome
-    this.usuario_nome = sessionSt.retrieve('token').user.email;
-  }
 }
