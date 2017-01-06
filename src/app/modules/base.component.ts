9d78686345e9bc00b5ba3bd792fd43fb0c706064
Leandro Machado Pereira <llpereiras@gmail.com>
2017-01-06 11:55:14 -0200

    Incluindo FlashMessageService ao baseComponent

--- src/app/modules/base.component.ts
+++ src/app/modules/base.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { ApiService } from './../service/api.service';
 import { SessionStorageService } from 'ng2-webstorage';
 import { ServiceLocator } from './../service_locator';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 export class BaseComponent {
 
@@ -9,11 +10,13 @@ export class BaseComponent {
   protected _api: any;
   protected _router: any;
   protected _sessionSt:any;
+  protected _flashMessagesService:any;
 
   constructor() {
     this._api = ServiceLocator.injector.get(ApiService);
     this._router = ServiceLocator.injector.get(Router);
     this._sessionSt = ServiceLocator.injector.get(SessionStorageService);
+    this._flashMessagesService = ServiceLocator.injector.get(FlashMessagesService);
   }
 
   get api(): ApiService {
@@ -26,6 +29,10 @@ export class BaseComponent {
 
   get sessionSt(): SessionStorageService {
     return this._sessionSt;
+  }
+
+  get flashMessagesService(): FlashMessagesService {
+    return this._flashMessagesService;
   }
 
 }
