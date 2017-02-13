8e02298269124b9dec86c7dd7c9df3dd0a583a45
Leandro Machado Pereira <llpereiras@gmail.com>
2017-02-13 17:27:12 -0200

    Alterando component de convidado para que seja possível cadastrar outros convidados na tela de eventos_convidados

--- src/app/modules/convidados/convidados.create.component.ts
+++ src/app/modules/convidados/convidados.create.component.ts
@@ -1,28 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { ConvidadoForm } from './convidados.form';
 import { BaseComponent } from './../base.component';
+import { EventosAcompanhantesComponent } from './../eventos_acompanhantes/eventos_acompanhantes.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
+  selector: 'convidados-create',
   templateUrl: './convidados.save.html'
 })
 export class ConvidadoCreateComponent extends BaseComponent{
   title = 'Convidado Novo';
   convidado: any = {};
   formConvidado: any;
+  active_route: any;
+  @Input() acompanhante: string;
 
-  constructor(convidadoform:ConvidadoForm){
+  constructor(convidadoform:ConvidadoForm, active_route: ActivatedRoute){
     super();
     this.formConvidado = convidadoform.getForm();
   }
 
   save () {
+
+    if (this.acompanhante == 'acompanhante') {
+      this.formConvidado.controls['mostrar_listagem'].setValue(false);
+    }
+
     this.saving = true;
     this.convidado = {"convidado": this.formConvidado.value};
+
     this.api.createConvidados(this.convidado).subscribe(
       (data: any) => {
-        this.flashMessagesService.show('Convidado salvo com sucesso!', { cssClass: 'alert-success', timeout: 5000 });
-        this.router.navigate(['convidados']);
+        if (this.acompanhante == 'acompanhante') {
+          let eva = new EventosAcompanhantesComponent(this.active_route);
+          eva.updateListAcompanhantes(this.api);
+        } else {
+          this.flashMessagesService.show('Convidado salvo com sucesso!', { cssClass: 'alert-success', timeout: 5000 });
+          this.router.navigate(['convidados']);
+        }
       },
       err => {
         this.saving = false;
